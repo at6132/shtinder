1efaf3a561060afb9bf23a27443f536da0e185d7
at6132 <avitaub6@gmail.com>
2025-11-18 02:01:48 -0500

    Fixed port binding

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Railway works BEST with standalone mode
+  output: 'standalone',
   reactStrictMode: true,
   eslint: {
     // Allow build to continue even with warnings (img tags)
